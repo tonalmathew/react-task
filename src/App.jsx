b1a65a14911aa227e2da8543095027fe648070c1
Tonal Mathew <tonalmathew.dev@gmail.com>
2024-01-08 20:38:57 +0530

    chore: adjust margin bottom

--- src/App.jsx
+++ src/App.jsx
@@ -34,21 +34,21 @@ function App() {
 					</button>
 				</div>
 				<div className='sm:m-20 md:flex md:gap-10 mt-32'>
-					<div className='md:w-1/3'>
+					<div className='md:w-1/3 mb-32 md:mb-10'>
 						<img src={img1} className='w-full h-auto' alt='image 1' />
 						<p className='text-center font-roboto font-light mt-5'>
 							Explore large, destructible environments where no two games are
 							ever the same.
 						</p>
 					</div>
-					<div className='md:w-1/3'>
+					<div className='md:w-1/3 mb-32 md:mb-10'>
 						<img src={img2} className='w-full h-auto' alt='image 2' />
 						<p className='text-center font-roboto font-light mt-5'>
 							Team up with friends by sprinting, climbing, and smashing your way
 							to earn your Victory Royale.
 						</p>
 					</div>
-					<div className='md:w-1/3'>
+					<div className='md:w-1/3 mb-32 md:mb-10'>
 						<img src={img3} className='w-full h-auto' alt='image 3' />
 						<p className='text-center font-roboto font-light mt-5'>
 							Discover even more ways to play across thousands of creator-made
